09f2909ec268875b7e099cec2930ecf3b1203c7a
Pedro <pedrohenrique2604@gmail.com>
2019-05-05 18:25:40 -0300

    MoveFactory class... changed the times a move can be used.

--- src/aps/MoveFactory.java
+++ src/aps/MoveFactory.java
@@ -14,15 +14,15 @@ public class MoveFactory {
     public static Move createMove(String name){
        switch(name){
             case "leafStorm":
-                return new Move("Leaf Storm", 30, PokemonAndMoveTypes.GRASS, 4);
+                return new Move("Leaf Storm", 30, PokemonAndMoveTypes.GRASS, 7);
             case "magmaStorm": 
-                return new Move("Magma Storm", 30, PokemonAndMoveTypes.FIRE, 5);
+                return new Move("Magma Storm", 30, PokemonAndMoveTypes.FIRE, 9);
             case "waterfall": 
-                return new Move("Waterfall", 20, PokemonAndMoveTypes.WATER, 5);
+                return new Move("Waterfall", 20, PokemonAndMoveTypes.WATER, 9);
             case "shockWave":
-                return new Move("Shock Wave", 35, PokemonAndMoveTypes.ELECTRIC, 3);
+                return new Move("Shock Wave", 35, PokemonAndMoveTypes.ELECTRIC, 5);
             case "skyAttack": 
-                return new Move("Sky Attack", 30, PokemonAndMoveTypes.FLYING, 4);
+                return new Move("Sky Attack", 30, PokemonAndMoveTypes.FLYING, 7);
        } 
         
        return null;
